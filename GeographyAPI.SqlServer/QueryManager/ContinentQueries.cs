69e8d2ebdf885bb23ec221f264dac9449c1a5e94
PatrickJones <hiramdeveloper@gmail.com>
2016-09-04 16:57:15 -0700

    Added EF database model

---
+++ GeographyAPI.SqlServer/QueryManager/ContinentQueries.cs
@@ -0,0 +1,24 @@
+﻿using GeographyAPI.SqlServer.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeographyAPI.SqlServer.QueryManager
+{
+    public class ContinentQueries : QueryBase
+    {
+        private GlobalStandardsEntities db = new GlobalStandardsEntities();
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ GeographyAPI.SqlServer/QueryManager/QueryBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeographyAPI.SqlServer.QueryManager
+{
+    public class QueryBase : IDisposable
+    {
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                Dispose();
+            }
+        }
+
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
