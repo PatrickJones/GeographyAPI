b156da92a0c9bc151194cf435364cd586ea2ccc6
PatrickJones <hiramdeveloper@gmail.com>
2016-09-04 15:17:29 -0700

    Add project files.

---
+++ GeographyAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace GeographyAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ GeographyAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GeographyAPI.WebApiApplication" Language="C#" %>
