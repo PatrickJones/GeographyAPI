1957356cfa1847decff08bea6ab58163cd3b6aa7
Patrick Jones <ptjhiram@outlook.com>
2016-10-06 19:54:20 -0700

    Added app-ang.js file

---
+++ GeographyAPI/Scripts/app-ang.js
@@ -0,0 +1,45 @@
+﻿var geoApp = angular.module("geoApp", []);
+
+var model = {
+    continents: [
+        { name: "North America" },
+        { name: "South America" },
+        { name: "Asia" },
+        { name: "Africa" },
+        { name: "Europe" }
+    ],
+    counrties: [
+        { name: "Country 1" },
+        { name: "Country 2" },
+        { name: "Country 3" }
+    ],
+    states: [
+        { name: "State 1" },
+        { name: "State 2" },
+        { name: "State 3" }
+    ],
+    cities: [
+        { name: "City 1", image: "../Content/img/slider/thumb1.jpg" },
+        { name: "City 2", image: "../Content/img/slider/thumb2.jpg" },
+        { name: "City 3", image: "../Content/img/slider/thumb3.jpg" }
+    ]
+};
+
+geoApp.controller("ContinentsCtrl", function ($scope) {
+    $scope.continents = model.continents;
+});
+
+geoApp.controller("CountriesCtrl", function ($scope) {
+    $scope.countries = model.counrties;
+});
+
+geoApp.controller("StatesCtrl", function ($scope){
+    $scope.states = model.states;
+});
+
+geoApp.controller("CitiesCtrl", function ($scope) {
+    $scope.cities = model.cities;
+    $scope.changeImage = function (city) {
+        console.log(city);
+    };
+});
